537a8695021edb168fe6ad13b14cdae807c489c6
BruceWong <herecomein@qq.com>
2025-01-10 21:11:41 +0800

    update

--- Crawler_newsukraine_war_in_ukraineV2.py
+++ Crawler_newsukraine_war_in_ukraineV2.py
@@ -1,11 +1,24 @@
+from PIL import Image
+from io import BytesIO
 from CrawlerBase import *
 from CrawlerBaseExclusion import *
 
 exclusion = """https://newsukraine.rbc.ua/
 ua https://www.rbc.ua/
+https://newsukraine.rbc.ua/about.shtml
+https://newsukraine.rbc.ua/termsofuse.shtml
+https://newsukraine.rbc.ua/legacy.shtml
+https://newsukraine.rbc.ua/static/principles/index.html
+https://newsukraine.rbc.ua/privacypolicy.shtml
+https://newsukraine.rbc.ua/contacts.shtml
+https://newsukraine.rbc.ua/becomeauthor.shtml
+https://newsukraine.rbc.ua/static/principles/index.html
+https://newsukraine.rbc.ua/static/command/index.html
 x https://x.com/NewsUkraineRBC
 en javascript:;
+https://whatsapp.com/channel/0029VaVYmRP0rGiPdNM12L2g
 https://newsukraine.rbc.ua/search
+https://twitter.com/NewsUkraineRBC
 https://www.facebook.com/NewsUkraineRBC/
 ru https://www.rbc.ua/ukr
 War in Ukraine https://newsukraine.rbc.ua/war-in-ukraine
@@ -37,11 +50,12 @@ Middle East conflict https://newsukraine.rbc.ua/tag/israel
 President Zelenskyy https://newsukraine.rbc.ua/tag/volodymyr-zelenskyy
 Follow us on X. Get the latest news https://x.com/NewsUkraineRBC
 """
+news_ukraine_keyword = "https://newsukraine.rbc.ua/news"
 OPEN = 1
 CLOSED = 0
 
 
-class CrawlerNewsUkraineRbcUa(CrawlerBase):
+class CrawlerNewsUkraineRbcUaNewsPage(CrawlerBase):
     def __init__(self, target_url, result_txt_file_name=None, crawl_number=15):
         super().__init__(target_url, result_txt_file_name)
         self.target_url = target_url
@@ -73,48 +87,6 @@ class CrawlerNewsUkraineRbcUa(CrawlerBase):
         self.crawl_only_today = condition
         pass
 
-    # def initiate_environment(self):
-    #     if not os.path.exists(self.result_directory):
-    #         os.makedirs(self.result_directory)
-    #         pass
-    #     pass
-
-    def crawl_main_page_urls(self):
-        urls_collection = []
-        # 设置请求头，模拟正常用户的浏览器请求
-        # self.headers_formed = {
-        #     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36',
-        #     'Accept-Language': 'en-US,en;q=0.5',
-        #     'Accept-Encoding': 'gzip, deflate, br',
-        #     'Connection': 'keep-alive',
-        #     'Upgrade-Insecure-Requests': '1',
-        #     'DNT': '1',  # Do Not Track 请求头
-        #     'Referer': self.target_url,  # 引用页，有助于避免被识别为爬虫（有时）
-        # }
-        response = requests.get(self.target_url, headers=self.headers_formed)
-        # 检查请求是否成功
-        if response.status_code == 200:
-            # 使用BeautifulSoup解析网页内容
-            soup = BeautifulSoup(response.text, 'html.parser')
-            links = soup.find_all('a')
-            for link in links:
-                # 获取链接的文本和URL
-                gotten_link = link.get('href')
-                # print(link.get_text(), link.get('href'))
-                if gotten_link in exclusion:
-                    pass
-                else:
-                    urls_collection.append((link.get_text().strip(), gotten_link))
-                    pass
-                pass
-
-            pass
-        else:
-            print(f"Error: {response.status_code}")
-            pass
-        pass
-        return urls_collection
-
     def crawl_news_page_urls(self):
         urls_collection = []
         prefix_reg = r'(\w+\s+\d+\D+\d+\D+\d+\:\d+\s+)(?=\w+)'
@@ -133,10 +105,13 @@ class CrawlerNewsUkraineRbcUa(CrawlerBase):
                 # dprint(clean_text)
                 # dprint('<'*100)
                 if gotten_link in exclusion:
-                    pass
+                    continue
+                elif news_ukraine_keyword not in gotten_link:
+                    continue
                 else:
-                    urls_collection.append((clean_text, gotten_link))
                     pass
+                urls_collection.append((clean_text, gotten_link))
+
                 pass
 
             pass
@@ -144,17 +119,20 @@ class CrawlerNewsUkraineRbcUa(CrawlerBase):
             print(f"Error: {response.status_code}")
             pass
         pass
-        # pprint(urls_collection)
+        print('urls_collection:')
+        pprint(urls_collection)
+        print('the number of crawled url links is {}'.format(str(len(urls_collection))))
+        while True:
+            cmd = input("verify the crawled urls ,are you sure to go on crawling ?(Y/N):")
+            if not cmd:
+                continue
+            elif cmd.lower() == 'y':
+                break
+            elif cmd.lower() == 'n':
+                exit(0)
+                pass
+            pass
         return urls_collection
-
-    def record_available(self):
-        if self.front_door_state == CLOSED and self.back_door_state == OPEN:
-            result = True
-            pass
-        else:
-            result = False
-            pass
-        return result
 
     def crawl_one_url_content(self, link_text, link_url, filename):
         crawl_result = True
@@ -165,6 +143,11 @@ class CrawlerNewsUkraineRbcUa(CrawlerBase):
         print('requesting link:', link_url)
         response = requests.get(link_url, headers=self.headers_formed)
         # 检查请求是否成功
+        try:
+            print("response.status_code:", response.status_code)
+            pass
+        except:
+            pass
         if response.status_code == 200:
             soup = BeautifulSoup(response.text, 'html.parser')
             # check date
@@ -179,17 +162,17 @@ class CrawlerNewsUkraineRbcUa(CrawlerBase):
             with open(filename, mode='a+', encoding='utf-8') as file:
                 file.write('\ntitle:\n')
                 # file.write(link_text)
-                file.write('    \n')
+                file.write('\n')
                 file.flush()
                 for element in soup.find_all(['h1', 'h2', 'h3', 'p', 'ul', 'li']):
                     if element.name in ['h1', 'h2', 'h3']:
                         file.write(element.text.strip())
-                        file.write('    \n')
+                        file.write('\n')
                         file.flush()
                         pass
                     elif element.name == 'p':
                         file.write(element.text.strip())
-                        file.write('    \n')
+                        file.write('\n')
                         file.flush()
                         pass
                     pass
@@ -199,27 +182,10 @@ class CrawlerNewsUkraineRbcUa(CrawlerBase):
             pass
         else:
             print(f"Error: {response.status_code}")
+            crawl_result = False
             pass
-        dprint('notice,crawled the page \n{}\n successfully!'.format(link_url))
+        dprint('notice,crawled the page \n{}\nsuccessfully!'.format(link_url))
         return crawl_result
-
-    def loop_crawl_main_page(self):
-        link_text_link_url_tuple_list = self.crawl_main_page_urls()
-        #
-        with open(self.result_abs_txt_file_name, 'w') as file:
-            file.write('')
-            pass
-        counter = 0
-        for link_text, link_url in link_text_link_url_tuple_list:
-            counter += 1
-            print('\ncrawling main page number {}/{}'.format(str(counter), str(self.crawl_number)))
-            self.crawl_one_url_content(link_text, link_url, self.result_abs_txt_file_name)
-            time.sleep(5)
-            if counter >= self.crawl_number:
-                break
-                pass
-            pass
-        pass
 
     def loop_crawl_news_page(self):
         self.link_text_link_url_tuple_list = self.crawl_news_page_urls()
@@ -228,19 +194,46 @@ class CrawlerNewsUkraineRbcUa(CrawlerBase):
         with open(self.result_abs_txt_file_name, 'w') as file:
             file.write('\ntitle:\n')
             pass
-        counter = 1
+        loop_counter = 1
         for link_text, link_url in self.link_text_link_url_tuple_list:
-            print('\ncrawling news page link number {}/{}'.format(str(counter), str(self.crawl_number)))
-            crawl_today_successfully = self.crawl_one_url_content(link_text, link_url, self.result_abs_txt_file_name)
-            time.sleep(5)
-            if not crawl_today_successfully:
+            print('\nThis page sequence number is {}'.format(str(loop_counter)))
+            if link_url in self.finished_url_list:
+                print('{}\nis already in finished url list.'.format(link_url))
+                self.logger.debug('found ({}) in finished record.'.format(link_url))
+                loop_counter += 1
+                self.logger.debug('counter add 1 to be {}.'.format(str(loop_counter)))
+                if loop_counter > self.crawl_number:
+                    print('Target crawl number reached!')
+                    break
+                    pass
                 continue
-            WebpagePictureDownloader.download_webpage_pictures(link_url, self.picture_download_directory)
-            if counter >= self.crawl_number:
+            print('\ncrawling news page link number {}/{}'.format(str(loop_counter), str(self.crawl_number)))
+            self.logger.debug('start crawling ({}).'.format(link_url))
+            crawl_this_successfully = self.crawl_one_url_content(link_text, link_url, self.result_abs_txt_file_name)
+            self.logger.debug('this crawl result:{}'.format(str(crawl_this_successfully)))
+            count_down_seconds(6)
+            if not crawl_this_successfully:
+                continue
+            # WebpagePictureDownloader.download_webpage_pictures(link_url, self.picture_download_directory)
+            print('Starting crawling pictures...')
+            WebpagePictureDownloader.download_webpage_pictures_of_the_size(link_url,
+                                                                           save_folder=self.picture_download_directory)
+            pass
+            self.finished_url_list.append(link_url)
+            finished_urls_text = '\n'.join(self.finished_url_list)
+            with open(self.finished_target_url_log_file, mode='w+', encoding='utf-8') as f:
+                f.write(finished_urls_text)
+                f.flush()
+                pass
+            self.logger.debug('crawl page ({}) and write record successfully.'.format(link_url))
+            loop_counter += 1
+            self.logger.debug('counter add 1 to be {}.'.format(str(loop_counter)))
+            if loop_counter > self.crawl_number:
+                print('Target crawl number reached!')
                 break
                 pass
-            counter += 1
-            time.sleep(5)
+            # time.sleep(6)
+            count_down_seconds(6)
             pass
         pass
 
@@ -272,17 +265,44 @@ pass
 
 
 def task1():
-    # target_url = 'https://newsukraine.rbc.ua/news'
     target_url = "https://newsukraine.rbc.ua/war-in-ukraine"
     referred_url = 'https://newsukraine.rbc.ua/'
     crawl_number = 20
-    ins = CrawlerNewsUkraineRbcUa(target_url, None, crawl_number)
+    ins = CrawlerNewsUkraineRbcUaNewsPage(target_url, None, crawl_number)
     ins.set_referred_url(referred_url)
     # ins.set_crawl_today(True)
     ins.run()
+    input('finished crawling,input any string to exit:')
     pass
 
 
+def task2():
+    url_list = [
+        "https://newsukraine.rbc.ua/static/img/_/t/_tramp_ze_4ce362f2a56fc0da5690a0e52f024de5_1300x820_7d46981567ab4506ec093426c0b48a28_260x164.jpg",
+        "https://newsukraine.rbc.ua/static/img/_/t/_tramp_ze_4ce362f2a56fc0da5690a0e52f024de5_1300x820_7d46981567ab4506ec093426c0b48a28_650x410.jpg"
+    ]
+
+    # 使用requests下载图片
+    for url in url_list:
+        response = requests.get(url)
+        print('mark')
+        # 检查请求是否成功
+        if response.status_code == 200:
+            # 将图片内容载入到BytesIO中
+            image = Image.open(BytesIO(response.content))
+            # 获取图片的宽度和高度
+            width, height = image.size
+            print(f'图片宽度: {width}, 图片高度: {height}')
+        else:
+            print('图片下载失败')
+        pass
+        # sleep(3)
+        count_down_seconds(3)
+        pass
+    pass
+
+
+pass
 if __name__ == '__main__':
     task1()
-    pass
+pass
